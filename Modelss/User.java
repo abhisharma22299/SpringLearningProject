d0a8a9e8a367a5719ff3b3b11a0e38808b77b233
Abhishek Sharma <abhisheksharmabca2020@gmail.com>
2023-12-27 22:44:07 +0530

    Add files via upload

---
+++ Modelss/User.java
@@ -0,0 +1,140 @@
+package com.Exam.ExamPortal.Modelss;
+
+
+
+import
+		com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.*;
+
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="users")
+public class 	User {
+    @Id
+    @GeneratedValue(strategy =	GenerationType.AUTO)
+	private Long id;
+	private String userName;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String phone;
+	private String image;
+	private boolean enable=true;
+	private String password;
+	// one user has many roles
+	@OneToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER,mappedBy = "user")
+	@JsonIgnore
+	private Set<UserRole> userRoles=new HashSet<>();
+
+	public User() {
+	}
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	public User(Long id, String userName, String firstName, String lastName, String email, String phone, String image, boolean enable, String password) {
+		this.id = id;
+		this.userName = userName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.image = image;
+		this.enable = enable;
+		this.password = password;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+
+
+
+	
+}
